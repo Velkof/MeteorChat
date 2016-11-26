0aae5bd33bd62f27844aa86c292b9f70c4be4c8f
Marjan <velkof@outlook.com>
2016-11-26 18:54:33 +0100

    Added email verification

---
+++ server/methods/sendVerificationEmail.js
@@ -0,0 +1,11 @@
+// /**
+//  * Created by Marjan on 11/26/2016.
+//  */
+// Meteor.methods({
+//    sendVerificationLink(){
+//     let userId = Meteor.userId();
+//       if (userId){
+//          return Accounts.sendVerificationEmail();
+//       }
+//    }
+// });
